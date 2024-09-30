7554eab157ecaf073783ed8fa5bbd85c85e65ef4
sonle88123 <quangson88123@gmail.com>
2024-09-30 17:17:10 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         React đầu tiên
+         React version 2
         </p>
         <a
           className="App-link"
