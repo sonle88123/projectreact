0025b60bdcd50267639e55e21c8f7952cbfd44b3
sonle88123 <quangson88123@gmail.com>
2024-10-02 17:26:37 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-         React version 2
+          React version 2
         </p>
         <a
           className="App-link"
